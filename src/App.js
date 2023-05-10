bbe973f0ec15388a27b7442d8ce9b3d67fc9c3a0
Rajayne <ryann.j.nealey@gmail.com>
2023-05-09 17:57:55 -0700

    Import answers and choose random to display msg and color

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Magic Eight Ball</header>
-      <EightBall />
+      <EightBall answers={answers} />
     </div>
   );
 }
--- src/EightBall.js
+++ src/EightBall.js
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
 import "./EightBall.css";
 
-const EightBall = () => {
+const EightBall = ({ answers }) => {
+  const randomIndex = () => Math.floor(Math.random() * answers.length);
+  const [answer, setAnswer] = useState({
+    msg: "Think of a question?",
+    color: "black",
+  });
+  const reset = () => {
+    setAnswer({
+      msg: "Think of a question?",
+      color: "black",
+    });
+  };
+
+  function handleClick(e) {
+    setAnswer(answers[randomIndex()]);
+  }
+
   return (
-    <div className="EightBall">
-      <h4 className="EightBall-header">Think of a question </h4>
-    </div>
+    <>
+      <div
+        className="EightBall"
+        onClick={handleClick}
+        style={{ backgroundColor: answer.color }}
+      >
+        <h4 className="EightBall-header">{answer.msg}</h4>
+      </div>
+      <button className="EightBall-reset" onClick={reset}>
+        Reset
+      </button>
+    </>
   );
 };
 
